fa5b669ded776eaa61acb8a2c806712016deabaf
djjrussell <djjrussell@gmail.com>
2021-02-02 13:30:38 -0800

    some tests, old computer is making this difficult, need to put a pin in this for now

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,72 @@
+import { configure, mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import Main from "./components/Main";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  it("renders correctly", () => {
+    shallow(<Main />);
+  });
+
+  it("update price state on change", () => {
+    const setPrice = jest.fn();
+    const wrapper = mount(<Main />);
+    const handleChange = jest.spyOn(React, "useState");
+    handleChange.mockImplementation(price => [price, setPrice]);
+    wrapper
+      .find("#price")
+      .at(0)
+      .simulate("change");
+    expect(setPrice).toBeTruthy();
+  });
+
+  it("update make state on change", () => {
+    const setMake = jest.fn();
+    const wrapper = mount(<Main />);
+    const handleChange = jest.spyOn(React, "useState");
+    handleChange.mockImplementation(make => [make, setMake]);
+    wrapper
+      .find("#make")
+      .at(0)
+      .simulate("change");
+    expect(setMake).toBeTruthy();
+  });
+
+  it("update model state on change", () => {
+    const setModel = jest.fn();
+    const wrapper = mount(<Main />);
+    const handleChange = jest.spyOn(React, "useState");
+    handleChange.mockImplementation(model => [model, setModel]);
+    wrapper
+      .find("#model")
+      .at(0)
+      .simulate("change");
+    expect(setModel).toBeTruthy();
+  });
+
+  it("update income state on change", () => {
+    const setIncome = jest.fn();
+    const wrapper = mount(<Main />);
+    const handleChange = jest.spyOn(React, "useState");
+    handleChange.mockImplementation(income => [income, setIncome]);
+    wrapper
+      .find("#income")
+      .at(0)
+      .simulate("change");
+    expect(setIncome).toBeTruthy();
+  });
+
+  it("update credit state on change", () => {
+    const setCredit = jest.fn();
+    const wrapper = mount(<Main />);
+    const handleChange = jest.spyOn(React, "useState");
+    handleChange.mockImplementation(credit => [credit, setCredit]);
+    wrapper
+      .find("#credit")
+      .at(0)
+      .simulate("change");
+    expect(setCredit).toBeTruthy();
+  });
 });
